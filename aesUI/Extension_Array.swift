adcf4e800ce237c29ed6e70387c1d4a66bdf8ea8
Philipp <mail@philippdumke.net>
2022-11-04 23:24:56 +0100

    added extensions to Sting and Array to return a codepoint representation / the given String

---
+++ aesUI/Extension_Array.swift
@@ -0,0 +1,8 @@
+//
+//  Extension_Array.swift
+//  aesUI
+//
+//  Created by Philipp Dumke on 04.11.22.
+//
+
+import Foundation
---
+++ aesUI/Extension_String.swift
@@ -0,0 +1,8 @@
+//
+//  Extension_Sting.swift
+//  aesUI
+//
+//  Created by Philipp Dumke on 04.11.22.
+//
+
+import Foundation
